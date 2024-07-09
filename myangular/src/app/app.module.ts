9fc4ca63a56fc722629287896dbf67aa78215122
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-07-09 18:09:04 +0600

    myangular

--- myangular/src/app/app.module.ts
+++ myangular/src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
+import { LocationComponent } from './location/location.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { CreateLocationComponent } from './create-location/create-location.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent
+    StudentComponent,
+    LocationComponent,
+    CreateLocationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
+    provideHttpClient(
+      withFetch()
+    ),
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
